5f4c253ab791fcc87459a2a2c44d583fd2d3ff1c
Jamen <jamen.loh@gmail.com>
2014-07-30 13:24:46 +0800

    Add full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static Pages" do
   
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  
   describe "Home page" do
     
     it "should have the content 'Sample App'" do
@@ -10,10 +12,15 @@ describe "Static Pages" do
       expect(page).to have_content('Sample App')
     end
     
-    it "should have the title 'Home'" do
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+    
+    it "should not have a custom page title" do
           visit '/static_pages/home'
-          expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-      end
+          expect(page).not_to have_title('| Home')
+    end    
   end
   
   describe "Help page" do
@@ -25,8 +32,8 @@ describe "Static Pages" do
       
       it "should have the title 'Help'" do
             visit '/static_pages/help'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-          end
+            expect(page).to have_title("#{base_title} | Help")
+      end
     end
     
     describe "About page" do
@@ -38,7 +45,20 @@ describe "Static Pages" do
       
       it "should have the title 'About Us'" do
             visit '/static_pages/about'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-          end
+            expect(page).to have_title("#{base_title} | About Us")
+      end
+    end
+    
+    describe "Contacte" do
+
+      it "should have the content 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_content('Contact')
+      end
+      
+      it "should have the title 'Contact'" do
+            visit '/static_pages/contact'
+            expect(page).to have_title("#{base_title} | Contact")
+      end
     end
 end
